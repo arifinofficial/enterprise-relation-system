d4bb8ea12c4f150204424b07a7eb66405ff8c0f7
Arifin N <arifinofficial30@gmail.com>
2019-09-25 14:02:05 +0800

    migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('phone', 15);
-            $table->string('address', 100);
+            $table->string('phone', 15)->nullable();
+            $table->string('address', 100)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
